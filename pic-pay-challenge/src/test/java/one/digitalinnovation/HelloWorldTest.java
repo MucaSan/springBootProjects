e84e4ba36e7df7e9de31b96d73c50e4966d9d4b5
svillar <villar.mucasan@gmail.com>
2024-03-22 23:46:33 -0300

    test beforeall and afterall

--- pic-pay-challenge/src/test/java/one/digitalinnovation/HelloWorldTest.java
+++ pic-pay-challenge/src/test/java/one/digitalinnovation/HelloWorldTest.java
@@ -1,4 +1,29 @@
 package one.digitalinnovation;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
 public class HelloWorldTest {
+
+    @BeforeAll
+    static void testSomethingHere(){
+        System.out.println("Log before all tests!");
+    }
+
+    @Test
+    void verifyIfMessageReturnsTrue(){
+        HelloWorld message = new HelloWorld("Hello World");
+        Assertions.assertTrue(message.verifyMessage());
+    }
+    @Test
+    void verifyIfMessageReturnsFalse(){
+        HelloWorld message = new HelloWorld("Not Hello World");
+        Assertions.assertFalse(message.verifyMessage());
+    }
+    @AfterAll All
+    static void testAfterAll(){
+        System.out.println("Logs after all tests");
+    }
 }
